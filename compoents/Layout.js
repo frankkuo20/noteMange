c643bc9b4f6262ef9beb8a0e4a0a73900c0a5bb2
frankChin <frank65501830@gmail.com>
2017-08-08 00:44:53 +0800

    nav content class name md grid proable change"

--- compoents/Layout.js
+++ compoents/Layout.js
@@ -65,7 +65,7 @@ export default ({title, content}) => {
   return (
     <div>
       <Head>
-        <link rel='stylesheet' href='/static/react-md.light_blue-yellow.min.css' />
+        {/* <link rel='stylesheet' href='/static/react-md.light_blue-yellow.min.css' /> */}
         <link rel='stylesheet' href='../static/my.css' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
